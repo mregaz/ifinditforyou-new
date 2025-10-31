5e7cc80a657cbda7b6155287d7ff94d85721ec7c
mregaz <mregaz@gmail.com>
2025-10-31 16:56:03 +0100

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
+ {
   try {
     const { query, lang } = await req.json();
 
