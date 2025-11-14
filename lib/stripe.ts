25f4d856b5b594c7bfd724bccfacf27f1e0e7cc6
mregaz <mregaz@gmail.com>
2025-11-14 22:14:05 +0100

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,1 +1,11 @@
+// lib/stripe.ts
+import Stripe from "stripe";
+
+const secretKey = process.env.STRIPE_SECRET_KEYS;
+
+if (!secretKey) {
+  throw new Error("Missing STRIPE_SECRET_KEYS environment variable");
+}
+
+export const stripe = new Stripe(secretKey);
 
