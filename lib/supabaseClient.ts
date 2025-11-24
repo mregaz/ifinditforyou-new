b8df711dd4ec0455c7c1e4dda0cb92f4024fd216
mregaz <mregaz@gmail.com>
2025-11-24 07:44:24 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Factory: ogni volta che la chiami ottieni un client Supabase
+export function createSupabaseClient() {
+  return createClient(supabaseUrl, supabaseAnonKey);
+}
+
